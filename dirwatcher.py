10d1adb03b46cc6efafac25999a977c6a0fbb58e
Nico Alfonso <nicodalfonso@gmail.com>
2021-03-22 11:52:49 -0400

    Updates display of runtime
    Updates display of path in logger banner.
    Now shows full path, wrapped to not exceed character count.
    
    Updates Exception handler to log Error and raise SIGTERM.
    Excepetions now exit program.
    
    Adds epilogue to parser.
    
    Adds comments for clarity

--- dirwatcher.py
+++ dirwatcher.py
@@ -3,9 +3,10 @@
 Dirwatcher - A long-running program
 
 Given a directory, file extension, and a string of text to search for,
-This program will continually mointor the directory for files with the
+this program will continually monitor the directory for files with the
 given extension, and log every line of those files that contain the
-specified "magic text".
+specified "magic text". Files added to, or removed from the given directory
+will be logged accordingly.
 """
 
 __author__ = "Nico D Alfonso"
@@ -17,18 +18,22 @@ import argparse
 import logging
 import time
 import re
+import textwrap
 
+# creates flag to allow for program termination
 exit_flag = False
 
+# creates and formats logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
     "%(asctime)s %(name)s\t%(levelname)s\n[%(threadName)s  ] %(message)s"
-    )
+)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
+# creates dict of files: line_count watched in memory
 watched_files = {}
 
 
@@ -54,7 +59,7 @@ def detect_removed_files(files):
     for f in watched_files:
         if f not in files:
             logger.info(f"Removing {f} from watchlist")
-            del(watched_files[f])
+            del watched_files[f]
 
 
 def search_for_magic(file, lines, start_line, magic_string):
@@ -63,12 +68,14 @@ def search_for_magic(file, lines, start_line, magic_string):
     """
     for i, line in enumerate(lines[start_line:]):
         if re.search(magic_string, line):
-            logger.info(
-                f"{magic_string} found on line {start_line + i + 1} of {file}")
+            # adds 1 to output line number to account for 0 based indexing
+            logger.info(f"{magic_string} found on line {start_line + i + 1} of {file}")
 
 
 def watch_directory(path, magic_string, extension):
     """
+    Controller function.
+
     Scans files in target directory, calls functions to monitor new and removed
     files, calls function to look for "magic text" if target files have been
     updated. Updates local memory's reference to last known line where
@@ -79,18 +86,17 @@ def watch_directory(path, magic_string, extension):
     global watched_files
 
     if os.path.exists(path):
-        files = [f for f in os.listdir(path) if f.endswith(extension)]
-        detect_added_files(files)
-        detect_removed_files(files)
-        for file in files:
+        current_files = [f for f in os.listdir(path) if f.endswith(extension)]
+        detect_added_files(current_files)
+        detect_removed_files(current_files)
+        for file in current_files:
             with open(os.path.join(path, file)) as f:
                 lines = f.readlines()
                 f.close()
             number_of_lines = len(lines)
+            # checks to see if files have been added to
             if watched_files[file] < number_of_lines:
-                search_for_magic(
-                    file, lines, watched_files[file], magic_string
-                    )
+                search_for_magic(file, lines, watched_files[file], magic_string)
                 watched_files[file] = number_of_lines
     else:
         logger.warning(f"directory {path} does not exist")
@@ -111,14 +117,23 @@ def create_parser(args):
     Provides help text to end user through CLI
     """
     parser = argparse.ArgumentParser()
+    parser.formatter_class = argparse.RawDescriptionHelpFormatter
     parser.add_argument("dir", help="the directory to watch")
-    parser.add_argument("--int", type=float, default=1.0,
-                        help="""how often to scan the directory, in seconds.
-                        Defaults to 1.0""")
-    parser.add_argument("ext",
-                        help="""extension for the type of files to be
-                        monitored for "magic text" (i.e., .txt, .log)""")
+    parser.add_argument(
+        "--int",
+        type=float,
+        default=1.0,
+        help="""how often to scan the directory, in seconds.
+                        Defaults to 1.0""",
+    )
+    parser.add_argument(
+        "ext",
+        help="""extension for the type of files to be
+                        monitored for "magic text" (i.e., .txt, .log, .doc)""",
+    )
     parser.add_argument("magic", help='the "magic text" to search for')
+    parser.epilog = """To exit this program, send either a SIGTERM or SIGINT signal.
+        HINT: ctrl + C"""
     ns = parser.parse_args(args)
     validate_ext(parser, ns.ext)
     return ns
@@ -138,7 +153,7 @@ def signal_handler(sig_num, frame):
     sig_name = signal.Signals(sig_num).name
 
     # log the associated signal name
-    logger.warning('Received ' + sig_name)
+    logger.warning("Received " + sig_name)
     if sig_name == "SIGTERM" or sig_name == "SIGINT":
         logger.info("Exiting")
         exit_flag = True
@@ -169,36 +184,39 @@ def main(args):
         "\n"
         "-------------------------------------------------------------------\n"
         "Beginning dirwatcher.py\n"
-        f"searching for {magic} in {dir}\n"
+        f"searching for {magic} in {textwrap.fill(path, 44)}\n"
         "-------------------------------------------------------------------"
-        )
+    )
 
     while not exit_flag:
         try:
             watch_directory(path, magic, ext)
         except Exception as e:
             # This is an UNHANDLED exception
-            # Log an ERROR level message here
-            logger.warning(e)
+            # Logs an ERROR level message here
+            logger.error(e)
+            # Raises SIGTERM to exit program
+            signal.raise_signal(signal.SIGTERM)
 
         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
         time.sleep(polling_interval)
 
     # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start
+    # Logs a message that we are shutting down
+    # Includes the overall uptime since program start
     runtime = time.perf_counter()
-    S = runtime % 100
+    S = runtime
     M = int(runtime) // 60
     H = M // 60
     logger.info(
         "\n"
         "-------------------------------------------------------------------\n"
         "Stopped dirwatcher.py\n"
-        f"Uptime was {H:02d}:{M:02d}:{S if S / 10 >= 1 else '0' + str(S)}\n"
+        f"Uptime was {H:02d}:{M:02d}:"
+        f"{S % 60 if S > 60 else S if S / 10 >= 1 else '0' + str(S)}\n"
         "-------------------------------------------------------------------\n"
-        )
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
